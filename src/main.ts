c42d042d19df09103c80aa6e3706d0a3fd61c932
MyatMinAung <myatminaung.dev@gmail.com>
2024-04-05 12:28:16 +0630

    fix(test): allow to run test before pushing

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  app.use(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   await app.listen(configService.get<number>('PORT'));
 }
